76f52205f845bbb20ecc22da97290b19cbc7d905
Agnes Wasikowska <aga@mcha.eu>
2019-10-19 18:13:23 +0100

    Generate noisy functions for the evaluation of the symbolic regression algorithm

--- __main__.py
+++ __main__.py
@@ -1,5 +1,23 @@
 from regression.symbollic_trees import GenerationParameters, SymbolicTreeGenerator, generate_population
+from evaluate.generate import generate_noisy_function
+import numpy as np
+import matplotlib.pyplot as plt
 
 gen_par = GenerationParameters(5, [1, 2, 3], ['divide', 'multiply'], 3)
 
 trees = generate_population(100, gen_par)
+
+'''
+Example usage of generate_noisy_funtion
+for sin:
+    x, y = generate_noisy_function(np.square, 0, 0.1, 100, 2 * np.pi)
+
+for exponential:
+    x, y = generate_noisy_function(np.square, 0, 1, 100, 10)
+
+To see the result of the noisy test sample run:
+    plt.plot(x, y, 'ro')
+    plt.show()
+
+'''
+
